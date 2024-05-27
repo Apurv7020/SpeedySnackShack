8685bb1106d311a8e8f40caf4bc417a5e76a5f37
Apurv Kalane <76426827+Apurv7020@users.noreply.github.com>
2024-05-27 22:03:39 +0530

    settings.py

--- speedysnackshack/settings.py
+++ speedysnackshack/settings.py
@@ -142,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'masterap26@gmail.com'
-EMAIL_HOST_PASSWORD = 'zngr ukzn zfbe gdom'
+EMAIL_HOST_USER = 'Your Email Address'
+EMAIL_HOST_PASSWORD = 'Your App Password'
